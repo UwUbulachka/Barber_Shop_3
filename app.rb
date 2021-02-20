9861dfa0e9ceade56d8cc11e19856ae2462beebe
UwUbulachka <meuw25@gmail.com>
2021-02-20 20:51:41 +0300

    начало

---
+++ app.rb
@@ -0,0 +1,45 @@
+#encoding: utf-8
+require 'rubygems'
+require 'sinatra'
+require 'sinatra/reloader'
+require "sinatra/activerecord"
+
+set :database, "sqlite3:barbershop.db"
+
+class Client < ActiveRecord::Base # создание сущности
+
+end
+
+class Barber < ActiveRecord::Base
+end	
+
+before do 
+	
+	@barbers = Barber.all
+
+end	
+
+
+get '/' do
+	
+	
+	erb :index
+end
+
+get '/visit' do
+	erb :visit
+end
+
+post '/visit' do
+
+	@username = params[:username]
+	@phone = params[:phone]
+	@datetime = params[:datetime]
+	@barber = params[:barber]
+	@color = params[:color]
+
+	
+
+	erb "<h2>Спасибо, вы записались!</h2>"
+
+end	
